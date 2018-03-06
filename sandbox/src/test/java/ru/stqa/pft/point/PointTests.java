2e1d1a24cb6500c745d1dff863e9fa7236369e4d
katerinas92 <e_suslova@mail.ru>
2018-03-06 18:20:27 +1000

    Изменен автотест для класса Point

--- sandbox/src/test/java/ru/stqa/pft/point/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/point/PointTests.java
@@ -7,9 +7,8 @@ public class PointTests {
 
   @Test
   public void testPointDistance() {
-    Point p = new Point(0,0);
     Point p1 = new Point(2, 6.8);
     Point p2 = new Point(3.6, 5.5);
-    Assert.assertEquals(p.distance(p1,p2), 2.0615528128088303);
+    Assert.assertEquals(p1.distance(p2), 2.0615528128088303);
   }
 }
