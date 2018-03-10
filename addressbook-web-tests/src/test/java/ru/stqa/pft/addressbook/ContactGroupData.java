afe55b472d94d00d20108a6f62ad1be69506679d
katerinas92 <e_suslova@mail.ru>
2018-03-10 21:45:27 +1000

    Рефакторинг для ContactCreationTests
    Части исходного кода выделены во вспомогательные методы (такие как переходы на страницы, нажатия кнопок, заполнение полей формы, авторизация и т.д.), а также создан вспомогательный класс groupCreationTests, который описывает объект типа "Контакт" и объекты этого типа имеют параметры: firstname, middlename, lastname, address, email, home, mobile, work.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactGroupData.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactGroupData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String address;
+  private final String email;
+  private final String home;
+  private final String mobile;
+  private final String work;
+
+  public ContactGroupData(String firstname, String middlename, String lastname, String address, String email, String home, String mobile, String work) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.address = address;
+    this.email = email;
+    this.home = home;
+    this.mobile = mobile;
+    this.work = work;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getWork() {
+    return work;
+  }
+}
