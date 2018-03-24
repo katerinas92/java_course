b1f46164590b80bb46d8033e903258d57c22a7c7
katerinas92 <e_suslova@mail.ru>
2018-03-25 01:49:19 +1000

    Лекция 3.4 - Сравнение объектов при помощи equals

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class Equality {
+
+  public static void main(String[] args) {
+    String s1 = "firefox";
+    String s2 = new String(s1);
+    String s3 = "firefox";
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+
+    System.out.println(s1 == s3);
+    System.out.println(s1.equals(s3));
+  }
+}
