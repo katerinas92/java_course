c2897415c33d63d360384a32fd0cefe1415e3ac6
katerinas92 <e_suslova@mail.ru>
2018-04-10 20:49:49 +1000

    Лекция 5.1 - Запуск браузера для выполнения тестов
    Изменено значение @BeforeMethod на @BeforeSuite, чтобы браузер запускался только один раз и выполнял все тесты друг за другом в одной вкладке браузера. Таким образом, тесты выполняются в 2 раза быстрее.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,21 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
   // передаем название браузера
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  // параметр static значает, что app будет доступен для всех тестов, запускаемых одновременно в BeforeSuite
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
