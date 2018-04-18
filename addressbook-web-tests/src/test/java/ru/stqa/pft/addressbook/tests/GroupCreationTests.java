621a16533a372bd75b027c1146112cd1beb1d34b
katerinas92 <e_suslova@mail.ru>
2018-04-18 23:10:00 +1000

    Лекция 6.4 - Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,23 +1,39 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 // Тест для создания новой группы контактов
 public class GroupCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    // Заполняем список массивом. Каждый массив содержит набор данных для одного запуска тестового метода.
+    List<Object[]> list = new ArrayList<Object[]>();
+    // Передаем в тест массивы с данными.
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    // Возвращаем итератор этого списка
+    return list.iterator();
+  }
+
   // тест на успешное создание новой группы
-  @Test
-  public void testGroupCreation() {
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     // Выбираем пункт меню "groups"
     app.goTo().groupPage();
     // Формируем множество из групп до создания новой
     Groups before = app.group().all();
-    // Задаем значения для новой группы
-    GroupData group = new GroupData().withName("test2");
     // Нажимаем кнопку "New group" для создания новой группы контактов; вызываем отдельный метод create()
     app.group().create(group);
     // проверка, что количество групп после добавления увеличилось на 1 с помощью hamcrest
