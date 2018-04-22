548f1ed2b91c41777151423e2392567ca0e2ec3b
katerinas92 <e_suslova@mail.ru>
2018-04-22 21:56:35 +1000

    Лекция 6.11 - Протоколирование (лог-файлы)
    Настроено логирование выполнения тестов с помощью библиотеки LOGBack на примере теста на создание группы.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -17,6 +19,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 // Тест для создания новой группы контактов
 public class GroupCreationTests extends TestBase {
+
+  Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
   // загрузка данных из файла XML
   @DataProvider
